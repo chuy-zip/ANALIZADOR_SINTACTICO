19233138ce2f87b20dd5bba25e4bf401a15753b5
chuy-zip <andreschuy03@gmail.com>
2025-05-23 23:58:31 -0600

    added example token list

---
+++ proyecto1/listas_tokens/tokens_manual_proy1.py
@@ -0,0 +1,22 @@
+[
+    {
+        "TokenName": "NUMBER",
+        "Lexema": "5"
+    },
+    {
+        "TokenName": "PLUS",
+        "Lexema": "+"
+    },
+    {
+        "TokenName": "NUMBER",
+        "Lexema": "5"
+    },
+    {
+        "TokenName": "TIMES",
+        "Lexema": "*"
+    },
+    {
+        "TokenName": "NUMBER",
+        "Lexema": "5"
+    }
+]
